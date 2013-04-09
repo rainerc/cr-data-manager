157fb85bb496ef374ea1c7537bedf4a89af63f38
rainer.clodius <rainer.clodius@19e93273-3609-7e11-9ceb-a1f47ea2af04>
2013-04-09 13:29:57 +0000

    change - Configurator form re-written change - basic Search functionality in Configurator

--- data-manager/configuratorForm.py
+++ data-manager/configuratorForm.py
@@ -1,97 +1,276 @@
-import clr
-import System
-clr.AddReference('System.Windows.Forms')
-from System.Windows.Forms import *
+﻿
+import System.Drawing
+import System.Windows.Forms
+
 from System.Drawing import *
-from System.IO import File
-from System.IO import FileInfo
+from System.Windows.Forms import *
 
 import globalvars
 import utils
-#from globalvars import DATFILE, ICON_SMALL, VERSION
 from utils import readFile
 from utils import ruleFile
 
+
 class configuratorForm(Form):
-	
 	def __init__(self):
-
+		self.InitializeComponent()
 		self.isDirty = False
-
 		self.theFile = ''
-
-		self.Width = 800
-		self.Height = 600
-		self.MaximizeBox = False
-		self.ShowIcon = True
-		self.Icon = Icon(globalvars.ICON_SMALL)
+		self.searchLabelText = 'search ...'
+	
+	def InitializeComponent(self):
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._buttonClose = System.Windows.Forms.Button()
+		self._statusStrip1 = System.Windows.Forms.StatusStrip()
+		self._toolStripStatusLabel1 = System.Windows.Forms.ToolStripStatusLabel()
+		self._toolStripStatusLabel2 = System.Windows.Forms.ToolStripStatusLabel()
+		self._toolStrip1 = System.Windows.Forms.ToolStrip()
+		self._buttonSave = System.Windows.Forms.Button()
+		self._toolStripStatusLabel3 = System.Windows.Forms.ToolStripStatusLabel()
+		self._buttonPlus = System.Windows.Forms.Button()
+		self._toolStripTextBox1 = System.Windows.Forms.ToolStripTextBox()
+		self._buttonFind = System.Windows.Forms.Button()
+		self._statusStrip1.SuspendLayout()
+		self._toolStrip1.SuspendLayout()
+		self.SuspendLayout()
+		# 
+		# textBox1
+		# 
+		self._textBox1.AcceptsReturn = True
+		self._textBox1.AcceptsTab = True
+		self._textBox1.Location = System.Drawing.Point(12, 28)
+		self._textBox1.Multiline = True
+		self._textBox1.Name = "textBox1"
+		self._textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+		self._textBox1.Size = System.Drawing.Size(760, 501)
+		self._textBox1.TabIndex = 0
+		self._textBox1.TabStop = False
+		self._textBox1.WordWrap = False
+		self._textBox1.Click += self.TextBox1Click
+		self._textBox1.KeyPress += self.TextBox1KeyPress
+		self._textBox1.KeyUp += self.TextBox1KeyUp
+		# 
+		# buttonClose
+		# 
+		self._buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+		self._buttonClose.Location = System.Drawing.Point(687, 537)
+		self._buttonClose.Name = "buttonClose"
+		self._buttonClose.Size = System.Drawing.Size(75, 23)
+		self._buttonClose.TabIndex = 1
+		self._buttonClose.Text = "Close"
+		self._buttonClose.UseVisualStyleBackColor = True
+		# 
+		# statusStrip1
+		# 
+		self._statusStrip1.Items.AddRange(System.Array[System.Windows.Forms.ToolStripItem](
+			[self._toolStripStatusLabel1,
+			self._toolStripStatusLabel2,
+			self._toolStripStatusLabel3]))
+		self._statusStrip1.Location = System.Drawing.Point(0, 532)
+		self._statusStrip1.Name = "statusStrip1"
+		self._statusStrip1.Size = System.Drawing.Size(784, 30)
+		self._statusStrip1.TabIndex = 2
+		self._statusStrip1.Text = "statusStrip1"
+		# 
+		# toolStripStatusLabel1
+		# 
+		self._toolStripStatusLabel1.AutoSize = False
+		self._toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom
+		self._toolStripStatusLabel1.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner
+		self._toolStripStatusLabel1.Name = "toolStripStatusLabel1"
+		self._toolStripStatusLabel1.Size = System.Drawing.Size(100, 25)
+		self._toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		# 
+		# toolStripStatusLabel2
+		# 
+		self._toolStripStatusLabel2.AutoSize = False
+		self._toolStripStatusLabel2.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom
+		self._toolStripStatusLabel2.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner
+		self._toolStripStatusLabel2.Name = "toolStripStatusLabel2"
+		self._toolStripStatusLabel2.Size = System.Drawing.Size(30, 25)
+		# 
+		# toolStrip1
+		# 
+		self._toolStrip1.Items.AddRange(System.Array[System.Windows.Forms.ToolStripItem](
+			[self._toolStripTextBox1]))
+		self._toolStrip1.Location = System.Drawing.Point(0, 0)
+		self._toolStrip1.Name = "toolStrip1"
+		self._toolStrip1.Size = System.Drawing.Size(784, 25)
+		self._toolStrip1.TabIndex = 3
+		self._toolStrip1.Text = "toolStrip1"
+		# 
+		# buttonSave
+		# 
+		self._buttonSave.Location = System.Drawing.Point(596, 537)
+		self._buttonSave.Name = "buttonSave"
+		self._buttonSave.Size = System.Drawing.Size(75, 23)
+		self._buttonSave.TabIndex = 4
+		self._buttonSave.Text = "Save"
+		self._buttonSave.UseVisualStyleBackColor = True
+		self._buttonSave.Click += self.ButtonSaveClick
+		# 
+		# toolStripStatusLabel3
+		# 
+		self._toolStripStatusLabel3.AutoSize = False
+		self._toolStripStatusLabel3.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom
+		self._toolStripStatusLabel3.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenInner
+		self._toolStripStatusLabel3.Name = "toolStripStatusLabel3"
+		self._toolStripStatusLabel3.Size = System.Drawing.Size(100, 25)
+		# 
+		# buttonPlus
+		# 
+		self._buttonPlus.Location = System.Drawing.Point(249, 537)
+		self._buttonPlus.Name = "buttonPlus"
+		self._buttonPlus.Size = System.Drawing.Size(75, 23)
+		self._buttonPlus.TabIndex = 5
+		self._buttonPlus.Text = "+"
+		self._buttonPlus.UseVisualStyleBackColor = True
+		self._buttonPlus.Click += self.ButtonPlusClick
+		# 
+		# toolStripTextBox1
+		# 
+		self._toolStripTextBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption
+		self._toolStripTextBox1.Name = "toolStripTextBox1"
+		self._toolStripTextBox1.Size = System.Drawing.Size(200, 25)
+		self._toolStripTextBox1.Text = "search ..."
+		self._toolStripTextBox1.Enter += self.ToolStripTextBox1Enter
+		self._toolStripTextBox1.Leave += self.ToolStripTextBox1Leave
+		self._toolStripTextBox1.KeyDown += self.ToolStripTextBox1KeyDown
+		self._toolStripTextBox1.DoubleClick += self.ToolStripTextBox1DoubleClick
+		# 
+		# buttonFind
+		# 
+		self._buttonFind.Location = System.Drawing.Point(237, 2)
+		self._buttonFind.Name = "buttonFind"
+		self._buttonFind.Size = System.Drawing.Size(75, 23)
+		self._buttonFind.TabIndex = 6
+		self._buttonFind.Text = "Find"
+		self._buttonFind.UseVisualStyleBackColor = True
+		self._buttonFind.Click += self.ButtonFindClick
+		# 
+		# configuratorForm
+		# 
+		self.ClientSize = System.Drawing.Size(784, 562)
+		self.Controls.Add(self._buttonFind)
+		self.Controls.Add(self._buttonPlus)
+		self.Controls.Add(self._buttonSave)
+		self.Controls.Add(self._toolStrip1)
+		self.Controls.Add(self._buttonClose)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._statusStrip1)
 		self.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-		self.Closing += self.formClosing
+		self.MaximizeBox = False
+		self.Name = "configuratorForm"
+		self.Text = "Form1"
+		self.FormClosing += self.ConfiguratorFormFormClosing
+		self.Load += self.ConfiguratorFormLoad
+		self._statusStrip1.ResumeLayout(False)
+		self._statusStrip1.PerformLayout()
+		self._toolStrip1.ResumeLayout(False)
+		self._toolStrip1.PerformLayout()
+		self.ResumeLayout(False)
+		self.PerformLayout()
 		
-		self.textbox = TextBox()
-		self.textbox.Location = Point(10, 10)
-		self.textbox.Width = 780
-		self.textbox.Height = 530
-		self.textbox.Multiline = True
-		self.textbox.ScrollBars = ScrollBars.Both
-		self.textbox.WordWrap = False
-		self.textbox.AcceptsTab = True
-		self.textbox.TabStop = False
-		self.textbox.Click += self.textBoxClick
-		self.textbox.KeyPress += self.textBoxClick
-		self.textbox.KeyDown += self.textBoxClick
+
+
+	def TextBox1Click(self, sender, e):
+		self.setLineInfo()
 		
-		self.statusLabel = Label()
-		self.statusLabel.Width = 70
-		self.statusLabel.Location = Point(100,545)
+	def setLineInfo(self):
+		line = self._textBox1.GetLineFromCharIndex(self._textBox1.SelectionStart) + 1
+		self._toolStripStatusLabel1.Text = 'Line %d' % line	
 
-		self.positionLabel = Label()
-		self.positionLabel.Width = 70
-		self.positionLabel.Location = Point(10,545)
-						
-		self.button1 = Button()
-		self.button1.Text = 'Save'
-		self.button1.Location = Point(580, 545)
-		self.button1.Width = 100
-		self.button1.Click += self.update
+	def TextBox1KeyPress(self, sender, e):
+		self.setLineInfo()
 
-		self.button2 = Button()
-		self.button2.Text = 'Close'
-		self.button2.Width = 100
-		self.button2.Location = Point(690, 545)
-
-		self.button2.DialogResult = DialogResult.Cancel
-		self.button2.Click += self.reset
-
-		self.Controls.Add(self.textbox)
-		self.Controls.Add(self.statusLabel)
-		self.Controls.Add(self.positionLabel)
-
-		self.addButtons()
-		self.showTheFile()
-		self.StartPosition = FormStartPosition.CenterParent
-
-		self.positionText('')
-
-	def textBoxClick (self, sender, event):
-		line = self.textbox.GetLineFromCharIndex(self.textbox.SelectionStart) + 1
-		self.positionText( '@ line %d' % line)
-
-	def positionText (self, s):
-		line = self.textbox.GetLineFromCharIndex(self.textbox.SelectionStart) + 1
-		self.positionLabel.Text = '@ line %d' % line
-
+	def TextBox1KeyUp(self, sender, e):
+		self.setLineInfo()
+		
 	def statusText(self, s):
 		if self.theFile == globalvars.DATFILE:
-			self.statusLabel.Text = s
+			self._toolStripStatusLabel3.Text = s
 
-	def update(self, sender, event):
+	def ButtonPlusClick(self, sender, e):
+		currentLine = self._textBox1.GetLineFromCharIndex(self._textBox1.SelectionStart)
+		self.setLineInfo()
+		tmp = self._textBox1.Text.split(System.Environment.NewLine)
+		tmp.insert(currentLine + 1,'New line %d' % currentLine)
+		self._textBox1.Text = System.Environment.NewLine.join(tmp)
+		i = 0
+		return
+	
+	def findString(self):
+		myText = str.lower(self._textBox1.Text)
+		try:
+			result = True
+			pos = myText.find(str.lower(self._toolStripTextBox1.Text), self._textBox1.SelectionStart + 1)
+#			if pos > 0:
+			self._textBox1.SelectionStart = pos
+			self._textBox1.SelectionLength = len(self._toolStripTextBox1.Text)
+			self._textBox1.Focus()
+			self._textBox1.ScrollToCaret()
+#			else:
+#				pos = 0
+#				self.findString()
+		except Exception, err:
+			MessageBox.Show('String not found')
+		self.setLineInfo()
+
+	def showTheFile(self):
+		if self.theFile <> '':
+			if self.theFile == globalvars.DATFILE:
+				ruleFile = utils.ruleFile()
+				self._textBox1.Text = ruleFile.read()
+				if ruleFile.editedByParser:
+					self.isDirty = True
+					self.statusText('* data changed')
+					MessageBox.Show('Your rules contained %d syntax errors. Those were marked with \"# invalid expression\"' % errlines)
+	
+			else:		
+				self._textBox1.Text = readFile(self.theFile)
+				
+	def textChanged(self, sender, event):
+		self.isDirty = True
+		self.statusText('* data changed')
+	
+	def setFile(self, f):
+		self.theFile = f
+		self.showTheFile()
+		self._textBox1.TextChanged += self.textChanged
+		self.setLineInfo()
+		if self.theFile <> globalvars.DATFILE:
+			self.buttonSave.Visible = False
+		return
+
+		
+	def ToolStripTextBox1DoubleClick(self, sender, e):
+		self.findString()
+
+	def ToolStripTextBox1Leave(self, sender, e):
+		if self._toolStripTextBox1.Text == '':
+			self._toolStripTextBox1.ForeColor = System.Drawing.Color.Gray
+			self._toolStripTextBox1.Text = self.searchLabelText
+		pass
+
+	def ToolStripButton1Click(self, sender, e):
+		self.findString()
+
+	def ConfiguratorFormLoad(self, sender, e):
+		self.Text = 'Data Manager for ComicRack - Version %s' % (globalvars.VERSION)
+		self.showTheFile
+		self._textBox1.SelectionStart = 1
+		self._textBox1.SelectionLength = 0
+		self._toolStripTextBox1.Text = self.searchLabelText
+		self._buttonPlus.Visible = False
+
+
+	def ButtonSaveClick(self, sender, e):
 		self.writeRuleFile()
 
 	def writeRuleFile(self):
 		self.Cursor = Cursors.WaitCursor
 		rulefile = utils.ruleFile()
-		if rulefile.write(self.textbox.Text) == rulefile.NOERROR:
+		if rulefile.write(self._textBox1.Text) == rulefile.NOERROR:
 			self.showTheFile()
 			self.isDirty = False
 			self.statusText('data saved')
@@ -103,53 +282,32 @@ class configuratorForm(Form):
 			# todo: more meaningfull error message
 			MessageBox.Show('Some rules were invalid! Those rules were marked.')
 		return not rulefile.editedByParser
-		
-	def formClosing(self, sender, event):
+
+	def ConfiguratorFormFormClosing(self, sender, e):
 		if self.isDirty and self.theFile == globalvars.DATFILE:
 			result = (MessageBox.Show(
-                   'Would you like to save your changes before closing?'
-                   , 'CR Data Manager - %s' % globalvars.VERSION
-                   , MessageBoxButtons.YesNoCancel
-                   , MessageBoxIcon.Question))
+               'Would you like to save your changes before closing?'
+               , 'CR Data Manager - %s' % globalvars.VERSION
+               , MessageBoxButtons.YesNoCancel
+               , MessageBoxIcon.Question))
 			if result == DialogResult.Yes:
 				if not self.writeRuleFile(): event.Cancel = True
 			elif result == DialogResult.No:
 				pass
 			elif result == DialogResult.Cancel:
 				event.Cancel = True
-	
-	def textChanged(self, sender, event):
-		self.isDirty = True
-		self.statusText('* data changed')
-		
-	def showTheFile(self):
-		if self.theFile <> '':
-			if self.theFile == globalvars.DATFILE:
-				ruleFile = utils.ruleFile()
-				self.textbox.Text = ruleFile.read()
-				if ruleFile.editedByParser:
-					self.isDirty = True
-					self.statusText('* data changed')
-					MessageBox.Show('Your rules contained %d syntax errors. Those were marked with \"# invalid expression\"' % errlines)
-	
-			else:		
-				self.textbox.Text = readFile(self.theFile)
 
-	def addButtons(self):
-		if self.theFile == globalvars.DATFILE:
-			self.Controls.Add(self.button1)
-			self.button2.TabStop = False
-			
-		self.Controls.Add(self.button2)
+	def ButtonFindClick(self, sender, e):
+		self.findString()
 
-	def setFile(self, f):
-		self.theFile = f
-		self.showTheFile()
-		self.textbox.TextChanged += self.textChanged
-		self.addButtons()
+	def ToolStripTextBox1Enter(self, sender, e):
+		self._toolStripTextBox1.ForeColor = System.Drawing.Color.Black
+		if self._toolStripTextBox1.Text == self.searchLabelText:
+			self._toolStripTextBox1.Text = ''
+		pass
+
+	def ToolStripTextBox1KeyDown(self, sender, e):
+		if e.KeyCode == Keys.Enter and self._toolStripTextBox1.Text <> '':
+			self.findString()
+			self._toolStripTextBox1.Focus()
 		
-	def setTitle(self, s):
-		self.Text = '%s - Version %s' % (s, globalvars.VERSION)
-		
-	def reset(self, sender, event):
-		self.Close
