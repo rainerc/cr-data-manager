f4b1d59c9c42f96bff97573d5d52709b97dccb6a
rainer.clodius <rainer.clodius@19e93273-3609-7e11-9ceb-a1f47ea2af04>
2013-04-09 15:51:09 +0000

    change - first rudimentary GUI written (no functionality yet)

---
+++ data-manager/Empty1.py
@@ -0,0 +1,15 @@
+﻿import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+
+from System.Windows.Forms import Application
+import configuratorForm
+
+import globalvars
+
+Application.EnableVisualStyles()
+form = configuratorForm.configuratorForm()
+#Application.Run(form)
+form.setFile(globalvars.DATFILE)
+form.ShowDialog()
+#form.Dispose()
