a22c8ef33a0aee0f3700d41194845c5ae583dd95
rainer.clodius <rainer.clodius@19e93273-3609-7e11-9ceb-a1f47ea2af04>
2013-04-10 18:35:24 +0000

    beta release r110

--- data-manager/globalvars.py
+++ data-manager/globalvars.py
@@ -16,9 +16,11 @@ ICON_SMALL = Path.Combine(FOLDER, 'dataMan16.ico')
 ICON = Path.Combine(FOLDER, 'dataMan.ico')
 IMAGE = Path.Combine(FOLDER, 'dataMan.png')
 IMAGESEARCH = Path.Combine(FOLDER, 'search.png')
+IMAGEADD = Path.Combine(FOLDER, 'add.png')
+IMAGEAPPLY = Path.Combine(FOLDER, 'Apply.png')
 DONATE = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UQ7JZY366R85S'
 WIKI = 'http://code.google.com/p/cr-data-manager/'
 MANUAL = 'http://code.google.com/p/cr-data-manager/downloads/list'
 
 COMPARE_CASE_INSENSITIVE = True
-VERSION = '0.1.15 r109'
+VERSION = '0.1.15 r110'
