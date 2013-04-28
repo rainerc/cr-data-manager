d73dba9977dd4589c35984ed6d65824e7dcdea02
rainer.clodius <rainer.clodius@19e93273-3609-7e11-9ceb-a1f47ea2af04>
2013-04-28 23:08:35 +0000

    fixed - progressbar shows progress while parsing rules fixed - label text of progressbar is not updating fixed - progressbar was not centered change - added allowedValModifiersNumeric to dataman.ini fixed - GUI crashes if called with path argument (issue 64, no path argument needed anymore)
    todo - chack valid modifiers in validate()
    todo - weird exceptions about missing indents in code generation

--- trunk/data-manager/Empty2.py
+++ trunk/data-manager/Empty2.py
@@ -4,38 +4,14 @@ clr.AddReference('System.Windows.Forms')
 clr.AddReference('System.Drawing')
 from System.IO import Path, FileInfo
 import globalvars
-from startupForm import startupForm
-
-def testIni():
-	import utils
-	from utils import *
-	
-	myIni = utils.iniFile(globalvars.INIFILE)
-
-	myIni.write('key1','val1')
-
-
-def theDllCall():
-	FOLDER = FileInfo(__file__).DirectoryName + "\\"
-	
-	theDLL = Path.Combine(FOLDER, 'crdmcgui.dll')
-	
-	#clr.AddReferenceToFileAndPath(theDLL)
-	clr.AddReference('crdmcgui.dll')
-	
-	from crdmcgui import gui
-	
-	
-	dmGUI = gui()
-	dmGUI.ShowDialog()
 	
 def showStartup():
-	from displayResultsForm import displayResultsForm
 	from progressForm import progressForm
-	from configuratorForm import configuratorForm
-	from aboutForm import aboutForm
-	theForm = aboutForm()
-	theForm.ShowDialog()
+	theForm = progressForm('sdffdf')
+	theForm.Show()
+
+
 	pass
-	
+
+
 showStartup()
