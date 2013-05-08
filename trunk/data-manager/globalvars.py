88b9b8335b95aa424be7c2598a686bc10df337a7
rainer.clodius <rainer.clodius@19e93273-3609-7e11-9ceb-a1f47ea2af04>
2013-05-08 21:27:27 +0000

    change - added Manga, LanguageISO and all YesNo fields (issue 58) change - added GUI r20

--- trunk/data-manager/globalvars.py
+++ trunk/data-manager/globalvars.py
@@ -13,7 +13,7 @@ ERRFILE = Path.Combine(FOLDER, 'dataMan.err')
 TMPFILE = Path.Combine(FOLDER, 'dataMan.tmp')
 LOGFILE = Path.Combine(FOLDER, 'dataMan.log')
 CHKFILE = Path.Combine(FOLDER, 'dataMan.chk')		# will be created once the configuration is saved
-GUIEXE = Path.Combine(FOLDER, 'crdmcgui-0.1.1r18.exe')
+GUIEXE = Path.Combine(FOLDER, 'crdmcgui.exe')
 
 ICON_SMALL = Path.Combine(FOLDER, 'dataMan16.ico')
 ICON = Path.Combine(FOLDER, 'dataMan.ico')
@@ -38,4 +38,4 @@ THECODE = []	# this will later on contain the parsed code
 
 
 COMPARE_CASE_INSENSITIVE = True
-VERSION = '1.0.4 r156'
+VERSION = '1.0.5 r161'
